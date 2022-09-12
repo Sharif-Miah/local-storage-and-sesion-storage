9ac561453daee162220c79a718df445a922936aa
farhan sharif <farhansharif1410@gmail.com>
2022-09-12 22:37:48 +0530

    local storage add

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ const storageValue = () => {
 
 }
 
+// Array set up in local Store
+
+const arr = ['Abir', 'Khalid', 'Osman', 'farhan'];
+const other = localStorage.setItem('name', JSON.stringify(arr))
+
+// Object set up in local Store
+
+const obj = {
+    id: 1,
+    name: 'Farhan',
+    work: 'Developer'
+}
+
+const objStore = localStorage.setItem('obj', JSON.stringify(obj));
